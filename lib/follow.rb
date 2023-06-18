04883cf968d3692a9215e737b78378b019e5c8a6
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2023-06-18 13:41:22 +0200

    Initial import

--- lib/follow.rb
+++ lib/follow.rb
@@ -3,6 +3,23 @@
 require_relative "follow/version"
 
 module Follow
-  class Error < StandardError; end
-  # Your code goes here...
+  class FollowEnumerator < Enumerator
+    def initialize(object, method = nil, &block)
+      closure = method ? lambda { |object| object.__send__(method) } : block
+      super() { |yielder|
+        while !object.nil?
+          yielder << object
+          object = closure.call(object)
+        end
+      }
+    end
+  end
+
+  def follow(object, method = nil, &block)
+    method.nil? == block_given? or raise ArgumentError, "Requires either a method argument or a block"
+    FollowEnumerator.new(object, method, &block)
+  end
+
+  module_function :follow
+  public :follow
 end
--- spec/follow_spec.rb
+++ spec/follow_spec.rb
@@ -3,5 +3,61 @@ describe Follow do
     expect(Follow::VERSION).not_to be_nil
   end
 
-  it 'does something useful'
+  describe "#follow" do
+    let(:klass) {
+      Class.new do
+        attr_reader :parent
+        def initialize(parent) = @parent = parent
+      end
+    }
+    
+    # a
+    #   b
+    #     c
+    # d
+    let(:a) { klass.new(nil) }
+    let(:b) { klass.new(a) }
+    let(:c) { klass.new(b) }
+    let(:d) { klass.new(d) }
+
+    it "returns an enumerator" do
+      expect(Follow.follow(a, :parent)).to be_a(Enumerator)
+    end
+
+    context "when object is nil" do
+      it "returns []" do
+        expect(Follow.follow(nil, :parent).to_a).to eq []
+      end
+    end
+
+    context "when object is not nil" do
+      it "includes the start-element" do
+        expect(Follow.follow(a, :parent).to_a).to eq [a]
+      end
+      it "considers 'false' to be a real value" do
+        enum = Follow.follow(false) { |value| value == false ? true : nil }
+        expect(enum.to_a).to eq [false, true]
+      end
+    end
+
+    context "with a method argument" do
+      it "follows the method" do
+        expect(Follow.follow(c, :parent).to_a).to eq [c, b, a]
+      end
+    end
+
+    context "with a block" do
+      it "follow the result of the block" do
+        enum = Follow.follow(c) { |obj| obj.parent }
+        expect(enum.to_a).to eq [c, b, a]
+      end
+    end
+
+    context "with conflicting arguments" do
+      it "raises an ArgumentError" do
+        expect { Follow.follow(a, :parent) { |arg| ; } }.to raise_error ArgumentError
+        expect { Follow.follow(a) }.to raise_error ArgumentError
+      end
+    end
+  end
 end
